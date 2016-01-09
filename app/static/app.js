cc1e26ab3446ea648e9c23ce32cc5fcbcffed8b1
Alex Huntley <a.p.huntley@btinternet.com>
2016-01-09 13:04:39 +0000

    editing and deleting functionality for web frontend

--- app/static/app.js
+++ app/static/app.js
@@ -1,19 +1,19 @@
-var app = angular.module('assuredWeb', ['ngRoute', 'ngResource', 'assuredWebControllers', 'assuredWebServices'])
-app.config(['$interpolateProvider', function($interpolateProvider) {
-  $interpolateProvider.startSymbol('[[');
-  $interpolateProvider.endSymbol(']]');
-}]);
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-	when('/tags', {
-	  templateUrl: 'static/partials/tag-list.html',
-	  controller: 'TagListCtrl'
-	}).
-	when('/tags/:tagId', {
-	  templateUrl: 'static/partials/tag-detail.html',
-	  controller: 'TagDetailCtrl'
-	}).
-	otherwise({
-	  redirectTo: '/tags'
-	});
-}]);
+	var app = angular.module('assuredWeb', ['ngRoute', 'ngResource', 'assuredWebControllers', 'assuredWebServices'])
+	app.config(['$interpolateProvider', function($interpolateProvider) {
+	  $interpolateProvider.startSymbol('[[');
+	  $interpolateProvider.endSymbol(']]');
+	}]);
+	app.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.
+		when('/tags', {
+		  templateUrl: 'static/partials/tag-list.html',
+		  controller: 'TagListCtrl'
+		}).
+		when('/tags/:tagId', {
+		  templateUrl: 'static/partials/tag-detail.html',
+		  controller: 'TagDetailCtrl'
+		}).
+		otherwise({
+		  redirectTo: '/tags'
+		});
+	}]);
--- app/static/controllers.js
+++ app/static/controllers.js
@@ -1,8 +1,24 @@
 var assuredWebControllers = angular.module('assuredWebControllers', [])
 
 assuredWebControllers.controller('TagListCtrl', function($scope, Tag) {
-  json = Tag.query()
-  $scope.tags = json
+  json = Tag.query();
+  $scope.tags = json;
+  $scope.onEdit = function(index) {
+	$scope.editedTag = Object.assign({}, $scope.tags[index]);
+	$scope.editedTagIndex = index;
+	$scope.editedTagId = $scope.tags[index].id;
+  }
+
+  $scope.saveEdit = function() {
+	Tag.update({ id: $scope.editedTag.id }, $scope.editedTag);
+	$scope.tags[$scope.editedTagIndex] = $scope.editedTag;
+  }
+
+  $scope.deleteTag = function() {
+	Tag.delete({ id: $scope.editedTagId });
+	$scope.tags.splice($scope.editedTagIndex, 1);
+  }
+  
 });
 
 assuredWebControllers.controller('TagDetailCtrl', function($scope, $routeParams, Tag) {
